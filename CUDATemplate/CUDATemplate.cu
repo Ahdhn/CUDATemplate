1938c74fc30b6b23acf137e1a0415f8ee63ae568
Ahmed <eng.a.hassen@gmail.com>
2020-02-13 17:43:26 -0800

    the template

---
+++ CUDATemplate/CUDATemplate.cu
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <assert.h>
+#include <cuda_runtime.h>
+
+
+__global__ static void testKernel()
+{
+    printf("\n I am thread %d", threadIdx.x);
+}
+
+int main(int argc, char **argv)
+{     
+    testKernel<<<1, 1>>>();
+    cudaDeviceSynchronize();
+    return EXIT_SUCCESS;
+}
+
