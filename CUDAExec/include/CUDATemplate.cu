8b7121f5674df7de9e196bd6b176fdfb7979c2a4
Ahmed Mahmoud <ahmahmoud@ucdavis.edu>
2022-03-14 11:36:45 -0400

    link to lib

--- CUDAExec/include/CUDATemplate.cu
+++ CUDAExec/include/CUDATemplate.cu
@@ -3,16 +3,24 @@
 #include <stdio.h>
 #include "gtest/gtest.h"
 
-
+#include "CUDALib.h"
 
 __global__ void exec_kernel()
 {
     printf("\n I am thread %d from exec_kernel\n", threadIdx.x);
 }
 
-TEST(Test, simple)
+TEST(Test, exe)
 {
     exec_kernel<<<1, 1>>>();
+    auto err = cudaDeviceSynchronize();
+    EXPECT_EQ(err, cudaSuccess);    
+}
+
+TEST(Test, lib)
+{
+    CUDALib lib;
+    lib.run();
     auto err = cudaDeviceSynchronize();
     EXPECT_EQ(err, cudaSuccess);
 }
